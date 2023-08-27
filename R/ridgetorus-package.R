62f7837428919bd4c705834c0f41900db182885d
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2023-08-27 17:34:45 +0200

    1.0.2 (#3)
    * Fix NOTEs + add reference
    
    * Update cran-comments.md
    
    * Fix surname
    
    * Update R-CMD-check
    
    * Update TR-PCA year on ReadMe
    
    * FIx Readme.md
    
    ---------
    
    Co-authored-by: Arturo Prieto Tirado <arpriet@gmail.com>

--- R/ridgetorus-package.R
+++ R/ridgetorus-package.R
@@ -8,12 +8,13 @@
 #' Cauchy distribution models and provides the associated scores and variance
 #' decomposition. Auxiliary functions for evaluating, fitting, and sampling
 #' these models are also provided. The package provides replicability to
-#' García-Portugués and Prieto-Tirado (2022) <arXiv:2212.10856>.
+#' García-Portugués and Prieto-Tirado (2023) <doi:10.1007/s11222-023-10273-9>.
 #'
 #' @author Eduardo García-Portugués and Arturo Prieto-Tirado.
 #' @references
-#' García-Portugués, E. and Prieto-Tirado, A. (2022). Toroidal PCA via density
-#' ridges. \emph{arXiv:2212.10856}. \url{https://arxiv.org/abs/2212.10856}
+#' García-Portugués, E. and Prieto-Tirado, A. (2023). Toroidal PCA via density
+#' ridges. \emph{Statistics and Computing}, 33(5):107.
+#' \doi{10.1007/s11222-023-10273-9}
 #' @docType package
 #' @name ridgetorus-package
 #' @import graphics grDevices stats Rcpp
